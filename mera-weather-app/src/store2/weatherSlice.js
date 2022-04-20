a23d007c4819838dc8e4fa4ada700eb1a4406a5d
OgiOrion <102956435+OgiOrion@users.noreply.github.com>
2022-04-20 15:08:45 +0200

    updated btn function for fetching from redux for updating a few sec ago icon in top right corner and description

--- mera-weather-app/src/store2/weatherSlice.js
+++ mera-weather-app/src/store2/weatherSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { init } from "i18next";
 
-const initialState = [
-  {
+const initialState = {
     current: {
       dew_point: 0,
       humidity: 0,
@@ -9,9 +9,17 @@ const initialState = [
       temp: 0,
       uvi: 0,
       visibility: 0,
+      weather: [{
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d"
+      }]
     },
-  },
-];
+    lat: 0,
+    lon: 0,
+    timezone: "nista"
+  };
 
 const weatherSlice = createSlice({
   name: "weather",
@@ -19,7 +27,7 @@ const weatherSlice = createSlice({
   reducers: {
     weatherAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        return action.payload;
       },
     },
   },
