c1172c6402d29c61818749934e26d5549c38562e
Teinovic <momir.teinovic.2011@gmail.com>
2022-04-28 22:34:43 +0200

    made the transition between 2 images
    -didn't yet set up loading spinner if the new image is taking too long to load
    -refactored moving props by using redux in hopes it would help with a re-render problem that occasionally happens when the image is starting to unmount

--- mera-weather-app/src/store2/index.js
+++ mera-weather-app/src/store2/index.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import weatherReducer from './weatherSlice';
-
+import weatherReducer from "./weatherSlice";
+import currentCityReducer from "./currentCitySlice";
 
 const store = configureStore({
-    reducer: {
-        weather: weatherReducer,
-    }
-})
+  reducer: {
+    weather: weatherReducer,
+    currentCity: currentCityReducer,
+  },
+});
 
 export default store;
